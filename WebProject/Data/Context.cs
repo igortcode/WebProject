474528887cd9486d67d0eba308d734f48ee9be94
igortorres17 <igor.s.torres@outlook.com>
2021-09-17 16:00:30 -0300

    Configuração da conexão com MySql e Scaffolded Item de Department

---
+++ WebProject/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProject.Models;
+
+namespace WebProject.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Department> departments { get; set;}
+
+        public Context(DbContextOptions<Context> options) : base(options){}
+    }
+}
